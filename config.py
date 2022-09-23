3c6e2fc0a043b03c28261ff6e28f5e1dac392e08
Rahul Bachina <bachina3@illinois.edu>
2022-09-22 21:40:25 -0500

    Added membershiplist class

--- config.py
+++ config.py
@@ -7,16 +7,18 @@ PING_TIMEOOUT: final = 0.5
 
 PING_DURATION: final = 1.5
 
-H1: final = Node('127.0.0.1', 8001)
-H2: final = Node('127.0.0.1', 8002)
-H3: final = Node('127.0.0.1', 8003)
-H4: final = Node('127.0.0.1', 8004)
-H5: final = Node('127.0.0.1', 8005)
-H6: final = Node('127.0.0.1', 8006)
-H7: final = Node('127.0.0.1', 8007)
-H8: final = Node('127.0.0.1', 8008)
-H9: final = Node('127.0.0.1', 8009)
-H10: final = Node('127.0.0.1', 8010)
+CLEANUP_TIME: final = 4.5
+
+H1: final = Node('127.0.0.1', 8001, 'H1')
+H2: final = Node('127.0.0.1', 8002, 'H2')
+H3: final = Node('127.0.0.1', 8003, 'H3')
+H4: final = Node('127.0.0.1', 8004, 'H4')
+H5: final = Node('127.0.0.1', 8005, 'H5')
+H6: final = Node('127.0.0.1', 8006, 'H6')
+H7: final = Node('127.0.0.1', 8007, 'H7')
+H8: final = Node('127.0.0.1', 8008, 'H8')
+H9: final = Node('127.0.0.1', 8009, 'H9')
+H10: final = Node('127.0.0.1', 8010, 'H10')
 
 GLOBAL_RING_TOPOLOGY: dict = {
 
--- membershipList.py
+++ membershipList.py
@@ -2,20 +2,49 @@ from nodes import Node
 import time
 from nodes import Node
 
+from config import CLEANUP_TIME
+
 class MemberShipList:
 
     def __init__(self, node: Node):
-        self.memberShipList = {}
+        self.memberShipListDict = {}
         self.self_node: Node = node
+    
+    def _cleanup(self):
+
+        for key in self.memberShipListDict.keys():
+            node_curr_time, node_curr_status = self.memberShipListDict[key]
+
+            if not node_curr_status and (time.time() - node_curr_time >= CLEANUP_TIME):
+                print(f'failure detected: {key.name}')
+                del self.memberShipListDict[key]
 
     def get(self):
-        node_data = (time.time(), 1)
-        # DO A CLEANUP
-        self.memberShipList[self.self_node] = node_data
-        return self.memberShipList
+        self._cleanup()
+        self.memberShipListDict[self.self_node] = (time.time(), 1)
+        return self.memberShipListDict
 
-    def update(self, new_membership_list: dict):
-        pass
+    def update(self, new_membership_list: dict) -> None:
 
+        isNewNodeAddedToList = False
+        for key in new_membership_list.keys():
+
+            if key in self.memberShipListDict:
+
+                new_time, new_status = new_membership_list[key]
+                curr_time, curr_status = self.memberShipListDict[key]
+
+                if curr_time < new_time:
+                    self.memberShipListDict[key] = (new_time, new_status)
+
+            else:
+                self.memberShipListDict[key] = (new_time, new_status)
+
+                if new_status:
+                    isNewNodeAddedToList = True
     
+    def print(self):
+        print(f'current local membership list')
+        for key, value in self.memberShipListDict.items():
+            print(f'{key.name} : {value}')
 
